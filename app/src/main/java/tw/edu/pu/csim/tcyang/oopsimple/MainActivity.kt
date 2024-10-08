51929ad3bd7887d9c2209c61b8e2b63976bddd0b
tcyang1971 <tcyang@gm.pu.edu.tw>
2024-10-08 15:26:08 +0800

    v2_class&object

--- app/src/main/java/tw/edu/pu/csim/tcyang/oopsimple/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/oopsimple/MainActivity.kt
@@ -18,6 +18,18 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         var txv: TextView = findViewById(R.id.txv)
-        txv.text = "物件導向實例"
+        //txv.text = "物件導向實例"
+        var v1 = Two()
+        v1.x = 5
+        v1.y = 7
+        txv.text = v1.Sum().toString()
+    }
+}
+
+class Two{
+    var x:Int = 0
+    var y:Int = 0
+    fun Sum(): Int {
+        return x + y
     }
 }
